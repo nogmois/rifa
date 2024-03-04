f1f7311de9d470b7c9e4be5ce038b024e25b4379
Murilo831 <nmurilo164@gmail.com>
2024-03-04 09:32:35 -0300

    update

---
+++ dados.md
@@ -0,0 +1,7 @@
+
+""" ENVIO DE SMS """
+"""
+def send_test_sms(request):
+    # Aqui você pode usar a função format_number_for_sms se necessário
+    formatted_number = format_number_for_sms('(123) 456-7890')
+    result = send_sms_via_textbelt(formatted_number, 'Olá, este é um teste de SMS do Django!')"""
--- main/views.py
+++ main/views.py
@@ -310,10 +310,3 @@ def alterar_horas(request, participacao_id):
         return HttpResponseBadRequest("Participação não encontrada")
     except (ValueError, KeyError):
         return HttpResponseBadRequest("Dados inválidos")
-
-""" ENVIO DE SMS """
-"""
-def send_test_sms(request):
-    # Aqui você pode usar a função format_number_for_sms se necessário
-    formatted_number = format_number_for_sms('(123) 456-7890')
-    result = send_sms_via_textbelt(formatted_number, 'Olá, este é um teste de SMS do Django!')"""
